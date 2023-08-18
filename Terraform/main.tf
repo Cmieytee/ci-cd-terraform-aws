704553d478c6aa40c49a663e6263afbfc304ff21
Cmieytee <129941983+Cmieytee@users.noreply.github.com>
2023-08-18 20:27:04 +0200

    Second commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,100 +5,60 @@ terraform {
       version = "~> 5.0"
     }
   }
-  backend "s3"{
+  backend "s3" {
     key = "aws/ec2-deploy/terraform.tfstate"
   }
 }
 
-provider "aws"{
-    region = var.region
+provider "aws" {
+  region = var.region
 }
 
-resource "aws_instance" "ubuntu-instance" {
-  ami = "ami-053b0d53c279acc90"
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.main_sg.id]
-  iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
-  connection {
-    type = "ssh"
-    host = self.public_ip
-    user = "ubuntu"
-    private_key = var.private_key
-    timeout = "4m"
+resource "aws_iam_instance_profile" "ec2-profile" {
+  name = "ec2-profile"
+  role = "EC2-ECR-auth"
+}
 
-  }
-  tags = {
-    "name" = "DeployVM"
-  }
-
-  resource "aws_iam_instance_profile" "ec2-profile" {
-    name = "ec2-profile"
-    role = "EC2-ECR-auth"
-  }
-
-
-  resource "aws_key_pair" "deployer"{
-    key_name = var.key_name
-    public_key = var.public_key
-  }
-
-  }
-
-
-  # security group
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key_name
+  public_key = var.public_key
+}
 
 resource "aws_security_group" "main_sg" {
   name        = "terraform-main"
   description = "Security group using Terraform"
   vpc_id      = "vpc-0e6c44411c7e5483a"
 
-  ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
- ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
- ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+  # Ingress and egress rules...
 
   tags = {
     Name = "TF_main"
   }
 }
 
-output "instance_public_ip"{
-
-    value = aws_instance.ubuntu-instance.public_ip
-    sensitive = true
-
-
+resource "aws_instance" "ubuntu-instance" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.main_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = var.private_key
+    timeout     = "4m"
+  }
+  
+  tags = {
+    "name" = "DeployVM"
+  }
 }
+
+output "instance_public_ip" {
+  value     = aws_instance.ubuntu-instance.public_ip
+  sensitive = true
+}
+
 
 
--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -1,15 +1,17 @@
 variable "region" {
-    default = "us-east-1"
+  description = "The AWS region where resources will be created."
+  default     = "us-east-1"
 }
 
-variable "public_key"{
-
+variable "public_key" {
+  description = "The public SSH key used for instance authentication."
 }
 
-variable "private_key"{
-
+variable "private_key" {
+  description = "The private SSH key corresponding to the public key."
 }
 
-variable "key_name"{
-    
+variable "key_name" {
+  description = "The name of the AWS EC2 key pair."
 }
+
